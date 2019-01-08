d40feba7d7ca806bda2ed6905240af5f3e4ba99c
Alejandro Santillan <asantillan@modoole.com>
2019-01-08 15:55:32 -0700

    [ADD] Unit15, Frame Class

---
+++ Python/Unit15-Tkinter/Tk.py
@@ -0,0 +1,32 @@
+from tkinter import *
+
+root = Tk()
+
+
+root.title("Hello world")
+root.resizable(True, True)
+root.iconbitmap('hola.ico')
+
+
+frame = Frame(root, width=480, height=320)
+
+# frame.pack(side="right", anchor="n")
+# frame.pack(fill='y', expand=1)
+
+frame.pack(fill='both', expand=1)
+
+frame.config(cursor="pirate")
+frame.config(bg="lightblue")
+frame.config(bd=25)
+frame.config(relief="sunken")
+
+
+root.config(cursor="arrow")
+root.config(bg="blue")
+root.config(bd=15)
+root.config(relief="ridge")
+
+
+# it must to be at final
+root.mainloop()
+
