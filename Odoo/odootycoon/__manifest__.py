6d0dfc113b00f765b56b6ef37594fd3c360619df
Alejandro Santillan <asanillan@modoole.com>
2019-02-05 18:03:24 +0000

    [ADD] Odoo, Game Manager Module

--- Odoo/odootycoon/__manifest__.py
+++ Odoo/odootycoon/__manifest__.py
@@ -1,30 +1,26 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "/workspace/odootycoon",
+    'name': "Odootycoon",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        Create a fun business Tycoon Game using Odoo's Framework""",
 
     'description': """
         Long description of module's purpose
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': "Modoole Tech",
+    'website': "https://www.modoole.com",
 
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
     'category': 'Uncategorized',
-    'version': '0.1',
+    'version': '12.0.0.1',
 
     # any module necessary for this one to work correctly
     'depends': ['base'],
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
     ],
