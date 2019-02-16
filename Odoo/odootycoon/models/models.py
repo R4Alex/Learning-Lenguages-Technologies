c60a195d2d9e3bcff0027acf9a9d893f1b3e54fa
Alejandro Santillan <asantill90@gmail.com>
2019-02-16 20:46:41 +0000

    [ADV] Odoo: Odootycoon, 4. Define Menu and Actions

--- Odoo/odootycoon/models/models.py
+++ Odoo/odootycoon/models/models.py
@@ -5,7 +5,7 @@ from odoo import models, fields, api
 class OdooTycoonGameManager(models.Model):
     _name = "odootycoon.gamemanager"
 
-    name = fields.Char("Game Name")
-    day = fields.Integer("Current Day")
-    cash = fields.Float("Cash")
+    name = fields.Char("Game Name", default="New Game")
+    day = fields.Integer("Current Day", default=1)
+    cash = fields.Float("Cash", default=5000)
     
