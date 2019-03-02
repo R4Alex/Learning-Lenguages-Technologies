cecfc8daa4143bcbd2e6e96a487d234ccc238d20
Alejandro Santillan <asantill90@gmail.com>
2019-03-02 18:30:34 +0000

    [ADV] Odoo Openacademy: Module Initialization

---
+++ odoo/openacademy/__manifest__.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Open Academy",
+
+    'summary': """
+        Short (1 phrase/line) summary of the module's purpose, used as
+        subtitle on modules listing or apps.openerp.com""",
+
+    'description': """
+        Long description of module's purpose
+    """,
+
+    'author': "Modoole Tech",
+    'website': "https://modoole.com",
+
+
+    'category': 'Uncategorized',
+    'version': '12.0.0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': [
+        'base'
+    ],
+
+    # always loaded
+    'data': [
+        'security/ir.model.access.csv',
+        'views/views.xml',
+        'views/templates.xml',
+    ],
+    
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+}
