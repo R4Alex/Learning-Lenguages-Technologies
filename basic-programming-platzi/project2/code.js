b48bb5843681d8cf3b2693952cef7147e1b171f0
Alejandro Santillan <asantillan@modoole.com>
2019-04-01 14:29:55 -0600

    Project 2.5: While and for

--- basic-programming-platzi/project2/code.js
+++ basic-programming-platzi/project2/code.js
@@ -1,12 +1,8 @@
-/*var route = window.location;
-console.log(console)
-document.write("hello student</br>")
-document.write("You are on: " + route)
-*/
 
 var element = document.getElementById("paint");
 var context = element.getContext("2d")
-
+var lines = 30;
+var l = 0;
 
 function drawLine(color, x_start, y_start, x_final, y_final) {
     context.beginPath();
@@ -14,8 +10,15 @@ function drawLine(color, x_start, y_start, x_final, y_final) {
     context.moveTo(x_start, y_start);
     context.lineTo(x_final, y_final);
     context.stroke();
-    context.closePath()    
+    context.closePath();
 }
 
-drawLine("blue", 10, 300, 220, 10)
-drawLine("yellow", 310, 10, 10, 220)
+//while (l < lines) {
+for (l = 0; l < lines; l++) {
+    y_start = 10 * l;
+    x_final = 10 * (l + 1);
+    drawLine("#AAF", 0, y_start, x_final, 300);
+}
+
+drawLine("black", 1, 1, 1, 299);
+drawLine("black", 1, 299, 299, 299);
