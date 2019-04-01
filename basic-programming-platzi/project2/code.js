77aa064072952a94fa7a81724a92629775a2ddc5
Alejandro Santillan <asantill90@gmail.com>
2019-03-31 23:18:36 -0600

    Project 2.3: DOM Painting

--- basic-programming-platzi/project2/code.js
+++ basic-programming-platzi/project2/code.js
@@ -1,4 +1,22 @@
-var route = window.location;
+/*var route = window.location;
 console.log(console)
 document.write("hello student</br>")
 document.write("You are on: " + route)
+*/
+
+var element = document.getElementById("paint");
+var context = element.getContext("2d")
+
+context.beginPath();
+context.strokeStyle = "red";
+context.moveTo(100, 100);
+context.lineTo(200, 200);
+context.stroke();
+context.closePath()
+
+context.beginPath();
+context.strokeStyle = "blue";
+context.moveTo(180, 10);
+context.lineTo(290, 200);
+context.stroke();
+context.closePath()
