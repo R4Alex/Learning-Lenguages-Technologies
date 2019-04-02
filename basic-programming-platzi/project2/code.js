d87c6b5d01a39d1ee18f5bee215aadc7f566996d
Alejandro Santillan <asantillan@modoole.com>
2019-04-02 13:02:29 -0600

    Project 2.5: Challenge

--- basic-programming-platzi/project2/code.js
+++ basic-programming-platzi/project2/code.js
@@ -1,24 +1,46 @@
-
 var element = document.getElementById("paint");
 var context = element.getContext("2d")
-var lines = 30;
+var lines = 60;
 var l = 0;
+var mycolor = "blue";
 
 function drawLine(color, x_start, y_start, x_final, y_final) {
     context.beginPath();
     context.strokeStyle = color;
+    context.lineWidth = 0.5;
     context.moveTo(x_start, y_start);
     context.lineTo(x_final, y_final);
     context.stroke();
     context.closePath();
 }
 
-//while (l < lines) {
 for (l = 0; l < lines; l++) {
     y_start = 10 * l;
     x_final = 10 * (l + 1);
-    drawLine("#AAF", 0, y_start, x_final, 300);
+    drawLine(mycolor, 0, y_start, x_final, 600);
 }
 
-drawLine("black", 1, 1, 1, 299);
-drawLine("black", 1, 299, 299, 299);
+for (l = 0; l < lines; l++) {
+    y_start = 10 * l;
+    x_final = 10 * (l + 1);
+    drawLine(mycolor, 600, y_start, x_final, 0);
+}
+
+for (l = 0; l < lines; l++) {
+    y_start = 10 * l;
+    x_final = 600 - (10 * (l + 1));
+    drawLine(mycolor, 600, y_start, x_final, 600);
+}
+
+for (l = 0; l < lines; l++) {
+    y_start = 600- (10 * l);
+    x_final = 10 * (l + 1);
+    drawLine(mycolor, 0, y_start, x_final, 0);
+}
+
+
+// Border
+drawLine("black", 1, 1, 1, 600);
+drawLine("black", 1, 600, 600, 600);
+drawLine("black", 1, 1, 600, 1);
+drawLine("black", 600, 1, 600, 600);
