50aea0018fe802eae8b8763eccdc441eb3ce8aa8
Alejandro Santillan <asantill90@gmail.com>
2019-05-11 14:46:39 -0600

    Keys project: Canvas piant with keyboard

--- basic-programming-platzi/keys/events.js
+++ basic-programming-platzi/keys/events.js
@@ -8,26 +8,49 @@ var keys = {
 console.log(keys);
 
 document.addEventListener("keydown", keyboardDraw);
+var canva = document.getElementById("paint");
+var context = canva.getContext("2d");
+var x = 150;
+var y = 150;
+var movement = 10;
+
+drawLine("browm", x-1, y-1, x+1, y+1, context);
+
+function drawLine(color, x_start, y_start, x_final, y_final, context) {
+  context.beginPath();
+  context.strokeStyle = color;
+  context.lineWidth = 3;
+  context.moveTo(x_start, y_start);
+  context.lineTo(x_final, y_final);
+  context.stroke();
+  context.closePath();
+}
 
 function keyboardDraw(event){
-
+  var littleColor = "blue";
   switch(event.keyCode){
     case keys.LEFT:
-      console.log("abajo");
+      drawLine(littleColor, x, y, x - movement, y, context);
+      x -= movement;
     break;
 
     case keys.UP:
-      console.log("abajo");
+      drawLine(littleColor, x, y, x, y - movement, context);
+      y -= movement;
     break;
 
     case keys.RIGHT:
-      console.log("abajo");
+    drawLine(littleColor, x, y, x + movement, y, context);
+    x += movement;
     break;
 
     case keys.DOWN:
-      console.log("abajo");
+      drawLine(littleColor, x, y, x, y + movement, context);
+      y += movement;
     break;
 
+    default:
+    break;
   }
 
 }
