5c81fd28dbb091b627db0f0462561d6b723111a2
Alejandro Santillan <asantill90@gmail.com>
2019-05-11 19:55:39 -0600

    [ADD] Mouse print challenge

--- basic-programming-platzi/4. mouse challenge/events.js
+++ basic-programming-platzi/4. mouse challenge/events.js
@@ -1,21 +1,3 @@
-var keys = {
-  LEFT: 37,
-  UP: 38,
-  RIGHT: 39,
-  DOWN: 40,
-};
-
-console.log(keys);
-
-document.addEventListener("keydown", keyboardDraw);
-var canva = document.getElementById("paint");
-var context = canva.getContext("2d");
-var x = 150;
-var y = 150;
-var movement = 10;
-
-drawLine("browm", x-1, y-1, x+1, y+1, context);
-
 function drawLine(color, x_start, y_start, x_final, y_final, context) {
   context.beginPath();
   context.strokeStyle = color;
@@ -26,31 +8,32 @@ function drawLine(color, x_start, y_start, x_final, y_final, context) {
   context.closePath();
 }
 
-function keyboardDraw(event){
-  var littleColor = "blue";
-  switch(event.keyCode){
-    case keys.LEFT:
-      drawLine(littleColor, x, y, x - movement, y, context);
-      x -= movement;
-    break;
-
-    case keys.UP:
-      drawLine(littleColor, x, y, x, y - movement, context);
-      y -= movement;
-    break;
-
-    case keys.RIGHT:
-    drawLine(littleColor, x, y, x + movement, y, context);
-    x += movement;
-    break;
-
-    case keys.DOWN:
-      drawLine(littleColor, x, y, x, y + movement, context);
-      y += movement;
-    break;
-
-    default:
-    break;
-  }
-
+function drawBorder(context){
+  drawLine("black", 0, 0, 0, canvas_height, context);
+  drawLine("black", 0, canvas_height, canvas_height, canvas_height, context);
+  drawLine("black", 0, 0, canvas_height, 0, context);
+  drawLine("black", canvas_height, 0, canvas_height, canvas_height, context);
 }
+
+var canDraw = false;
+function mouseDraw(event){
+  if(canDraw){
+      drawLine("blue", event.offsetX-1, event.offsetY-1, event.offsetX+1, event.offsetY+1, context);
+  }
+}
+
+var canva = document.getElementById("paint");
+var context = canva.getContext("2d");
+
+var canvas_width = canva.width;
+var canvas_height = canva.height;
+
+drawBorder(context);
+
+
+canva.addEventListener("mousemove", mouseDraw);
+canva.addEventListener("mousedown", function(){canDraw = true;});
+canva.addEventListener("mouseup", function(){canDraw = false;});
+
+
+
