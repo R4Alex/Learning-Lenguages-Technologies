2a41c72ea76e37c0167977cd893687c767b5f416
Alejandro Santillan <asanillan@modoole.com>
2019-08-26 02:03:09 +0000

    [add] Odoo, Openacademy: 13. Reports

--- odoo/openacademy/__manifest__.py
+++ odoo/openacademy/__manifest__.py
@@ -30,6 +30,7 @@
         'security/ir.model.access.csv',
         'views/views.xml',
         'views/partner.xml',
+        'reports/reports.xml',
     ],
     
     # only loaded in demonstration mode
