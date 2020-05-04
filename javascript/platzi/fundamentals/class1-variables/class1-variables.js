5c70f40c84914e4cfae37e3daf2dbb4c61293e1f
Alejandro Santillan <asantillan@modoole.com>
2020-05-04 17:46:30 -0600

    [INIT] Javascript Welcome

---
+++ javascript/platzi/fundamentals/class1-variables/class1-variables.js
@@ -0,0 +1,20 @@
+console.log("Hello Happy World")
+
+// var name
+// name = 'sacha'
+
+// var name = 'sacha'
+
+name = 'Alejandro'
+
+last_name = 'Santillan'
+
+age = 28
+
+// console.log('Hello ' + name)
+
+console.log('Hello ' + name + ' ' + last_name)
+
+console.log('I am ' + age + ' years old')
+
+var weight = 75
